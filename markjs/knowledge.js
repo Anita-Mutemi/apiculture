76c8051b4b8d09a0210a84d20b226be279a03c15
mitunda <132665739+mitunda@users.noreply.github.com>
2023-05-31 07:24:56 +0300

    Update knowledge.js
    updated js

--- markjs/knowledge.js
+++ markjs/knowledge.js
@@ -1,4 +1,4 @@
-// this is to add the to the slide-content class
+// this is to add the toggle effect to the slide-content class
 function toggleSlide(element) {
     var slideContent = element.querySelector('.slide-content');
     slideContent.classList.toggle('active');
